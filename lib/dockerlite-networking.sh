36eab72194747b3a0ed3873cad4c2631423fb2ef
Jérôme Petazzoni <jp@enix.org>
2013-05-09 09:23:01 -0700

    move all network allocation code in a single file

--- lib/dockerlite-networking.sh
+++ lib/dockerlite-networking.sh
@@ -37,3 +37,15 @@ _dl_setipaddr () {
     echo $CID > networks/$IPADDR/cid
     _dl_setc $CID ipaddr $IPADDR/24
 }
+
+_dl_cmd seta "manually allocate ip address to container"
+_dl_seta () {
+    CID=$1
+    IPADDR=$2
+    [ "$CID" ] || _dl_error "must specify container"
+    CID=$(_dl_resolve containers $CID)
+    if [ "$IPADDR" ]
+    then _dl_setipaddr $CID $IPADDR
+    else _dl_allocateipaddr $CID
+    fi
+}
